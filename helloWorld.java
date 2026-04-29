2a539d652066d830be7b12bc5c5929dc2ecbdea5
zubrist <zubrist7@gmail.com>
2026-04-29 20:24:14 +0530

    Ignore Java .class files

---
+++ helloWorld.java
@@ -0,0 +1,14 @@
+
+class helloWorld{
+
+    public static void main(String []args) {
+        System.out.println("Hello World");
+        
+    }
+}
+// comment lines in java
+// single 
+/*
+    multiline
+ */
+
