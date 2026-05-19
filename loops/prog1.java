ec6cca9a18d0da56e26079bd33fd93642cbefd1f
zubrist <zubrist7@gmail.com>
2026-05-19 18:52:15 +0530

    Java codes

---
+++ loops/prog1.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class prog1 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+         
+        for( int i =0 ; i<=10; i++){
+            sum = sum + i;
+            
+        }
+        System.out.println("The Total is : " +sum);
+        
+    }
+    
+}
---
+++ loops/prog2.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class prog2 {
+    public static void main(String[] args) {
+        String Fruits[] = {"Apple", "Mango", "Banana", "Orange" , "Litchi"};
+
+       // Fruits.length()
+
+
+        // For- each loop :  (dataType  variable : collection)
+
+        for(String fruit : Fruits){
+            System.out.println("Fruitt : " +fruit);
+        }
+        }
+    
+}
